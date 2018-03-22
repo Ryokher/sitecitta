caeac66358c8368077b96903eca4dade297d6426
DESKTOP-NBL3DAK\thoma <thomas.mahe@ynov.com>
2018-03-22 15:01:45 +0100

    page acceuil

---
+++ home.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Acceuil</title>
+</head>
+
+<body>
+    <header>
+        <div class="header">
+            <link href="css/home.css" rel="stylesheet">
+            <a class="hvr_boutton header_bt" href="#">ACCEUIL</a>
+            <a class="hvr_boutton header_bt" href="#">PRODUITS</a>
+            <a class="hvr_boutton header_bt" href="#">LE LOGO</a>
+            <a class="hvr_boutton header_bt" href="#">CONTACT</a>
+            <a class="hvr_boutton header_bt" href="#">A PROPOS</a>
+        </div>
+        <div class="barre_back"></div>
+    </header>
+    <div class="h_banner">
+    </div>
+    <div class="h_conteneur_block">
+        <div class="h_block"></div>
+        <div class="h_block1"></div>
+        <div class="h_block"></div>
+        <div class="h_block1"></div>
+        <div class="h_block"></div>
+        <div class="h_block1"></div>
+        <div class="h_block"></div>
+        <div class="h_block1"></div>
+        <div class="h_block"></div>
+        <div class="h_block1"></div>
+    </div>
+    <div class="h_about">
+        <div class="h_conteneur_about">
+            <p class="h_titre">ABOUT THE PRODUCT</p>
+            <div class="h_trait"></div>
+            <p class="h_desc">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
+        </div>
+    </div>
+    <div class="h_barre_triangle">
+        <div class="triangle"></div>
+    </div>
+
+    <div class="h_how">
+        <div class="h_conteneur_how">
+            <p class="h_titre">HOW DOES IT WORKS ?</p>
+            <div class="h_trait"></div>
+            <p class="h_desc">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
+        </div>
+    </div>
+    <div class="h_bloc"></div>
+</body>
+
+</html>
