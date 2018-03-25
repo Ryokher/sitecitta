90c8380dbdccfe06cd4c6ebf1848621546a490b9
DESKTOP-NBL3DAK\thoma <thomas.mahe@ynov.com>
2018-03-25 11:58:23 +0200

    je sait pas

---
+++ aboutus.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Accueil</title>
+</head>
+
+<body>
+    <header>
+        <div class="header">
+            <link href="css/about.css" rel="stylesheet">
+            <a class="hvr_boutton header_bt" href="#">ACCEUIL</a>
+            <a class="hvr_boutton header_bt" href="#">PRODUITS</a>
+            <a class="hvr_boutton header_bt" href="#">LE LOGO</a>
+            <a class="hvr_boutton header_bt" href="#">CONTACT</a>
+            <a class="hvr_boutton header_bt" href="#">A PROPOS</a>
+        </div>
+        <div class="barre_back"></div>
+    </header>
+    <div class="a_titre_ptincipale">
+    <p>ABOUT US</p>
+    </div>
+    <div class="a_banniere_img"></div>
+    <div class="a_barre_triangle">
+        <div class="triangle"></div>
+    </div>
+     <div class="a_about">
+        <div class="a_conteneur_about">
+            <p class="a_titre">ABOUT CITTA</p>
+            <div class="a_trait"></div>
+            <p class="a_desc">We create enduring design staples and limited edition seasonal collections for you and your home that are always fresh and original. So you can fill your colourful life with the things you love.
+Based on a relaxed antipodean philosophy of life and travel, our products are designed in New Zealand to be useful, beautiful and even a little bit fun.
+From India to New York, Tokyo and Barcelona, Città is inspired by the world's unique cultures. With each collection, we share a global design story, retold by you - through the way you live and bring its pattern, colour and texture into your home.
+We're based in Auckland, New Zealand, but you'll find seasonal and classic Città collections in discerning home and design retailers and department stores throughout the world</p>
+        </div>
+    </div>
+    <div class="a_barre_triangleg">
+        <div class="triangleg"></div>
+    </div>
+
+    <div class="a_how">
+        <div class="a_conteneur_how">
+            <p class="a_titre">ABOUT COMPAGNIE DE PROVENCE</p>
+            <div class="a_trait"></div>
+            <p class="a_desc">Il était une fois, en 1990, deux amis Marseillais, passionnés par la décoration et le design décident de revisiter une vieille tradition : l’emblématique cube du savon de Marseille. Conscients de la place qu’il occupe dans le patrimoine culturel et industriel en Provence, ils décident de le réinterpréter de manière moderne et contemporaine.
+
+Le savon de Marseille, qui fait la renommée et la fierté de sa ville depuis le XVIIème siècle, est un produit naturel fabriqué exclusivement à partir d’huiles végétales, sans aucun colorant ni adjuvant de synthèse. C’est cette exceptionnelle pureté qui lui confère ses vertus hypoallergéniques, antiseptiques et antibactériennes. C’est sur cette icône qu’ils construisent leur entreprise en s’attachant à ses valeurs d’authenticité, de simplicité, d’efficacité et de qualité.</p>
+        </div>
+    </div>
+     <footer>
+        <div class="bande"></div>
+        <div class="text">
+            <div class="textg">
+                <a href="#">Accueil</a>
+                <a href="#">Produits</a>
+            </div>
+            <div class="textd">
+                <a href="#">Contact</a>
+                <a href="#">A propos</a>
+            </div>
+        </div>
+        <div class="barreb"></div>
+        <div class="bas">
+                <a href="#"><img  src="img/logofb.png" alt="Notre Facebook" /></a>
+                <a  href="#"><img src="img/logotw.png" alt="Notre Twitter" /></a>
+                <a href="#"><img src="img/logol.png" alt="Notre Linkedin" /></a>
+        </div>
+        <div class="bande"></div>
+    </footer>
+    </body>
+</html>
